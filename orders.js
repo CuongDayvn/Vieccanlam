46aab85e8867f2d114add829233adf1ccb8bf529
CuongDay <Cuongcave085@gmail.com>
2024-04-28 17:24:17 +0700

    lưu ở đây

---
+++ orders.js
@@ -0,0 +1,27 @@
+const Orders = [
+  {
+    productName: "HTML+CSS",
+    productNumber: "01",
+    paymentStatus: "70%",
+    status: "Không Cần Nhớ - nhưng Phải quen Mắt Để tiến đến javaScript",
+  },
+  {
+    productName: "JavaScript Tutorial",
+    productNumber: "02",
+    paymentStatus: "30%",
+    status: "Nắm chắc được javaScript+HTML(Css) là bạn sắp học được python rồi",
+  },
+  {
+    productName: "Python",
+    productNumber: "03",
+    paymentStatus: "1%",
+    status: "Hoàn Thành Và Thực hiện nó",
+  },
+
+  {
+    productName: "Lấy Vợ",
+    productNumber: "04",
+    paymentStatus: "0%",
+    status: "Không Làm Được thì lấy Vợ Chăn Lợn",
+  },
+];
